daa6d8d7e0bb316e125f9ff30f582487c82ee936
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-31 15:48:23 +0530

    Updated Thymeleaf

--- src/main/java/com/springboot/thymeleaf/controllers/HelloController.java
+++ src/main/java/com/springboot/thymeleaf/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.springboot.thymeleaf.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -9,5 +10,12 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello"; //Name of the html file
+	}
+	
+	@RequestMapping("/sendData")
+	public ModelAndView sendData() {
+		ModelAndView mav = new ModelAndView("data"); //data is html file
+		mav.addObject("message","Take up one idea and make it your life");
+		return mav;
 	}
 }
